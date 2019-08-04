7c5c5bc79ad30a260240c2dde697b6b0a299b875
Amr Halim <50851126+Amr221B@users.noreply.github.com>
2019-08-04 23:39:36 +0200

    Update main.c

--- EPWM_CPU1/src/main.c
+++ EPWM_CPU1/src/main.c
@@ -9,6 +9,7 @@
 #include "epwm.h"
 
 void main(void){
+    // Initialize System Clock to 100 Mhz
     clkInitialize();
     // For 100 Khz PWM, 50% Duty Cycle
     EPWM1A_setPeriodAndDuty(2000, 0.5);
