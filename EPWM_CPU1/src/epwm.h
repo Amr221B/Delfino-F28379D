5dac32367a0a4e1b89cb87bd72d14457afaad235
Amr Halim <50851126+Amr221B@users.noreply.github.com>
2019-08-05 05:24:36 +0200

    Update epwm.h

--- EPWM_CPU1/src/epwm.h
+++ EPWM_CPU1/src/epwm.h
@@ -7,25 +7,37 @@
 
 #ifndef EPWM_EPWM_H_
 #define EPWM_EPWM_H_
-/* Prescaler to the ePWM modules.  Max ePWM input clock is 100 MHz.
- * epwm prescaler 0 or 1
+
+#include "F2837xD_device.h"
+
+/* ePWM Timer Base counter CLK
+ * T(ePWM Timer Base Clock) = PLLSYSCLK/(EPWM_PRESCALER * CLK_DIV * HIGH_SPEED_TB_CLK_PRESCALAR).
+ */
+
+/* Prescaler to the ePWM modules CLK; Max ePWM module input clock is 100 MHz.
+ * EPWM_PRESCALER 0 or 1
  * 0 -> epwm modules clk = PLLSYSCLK
  * 1 -> epwm modules clk = PLLSYSCLK/2
  */
-#include "F2837xD_device.h"
-
 #define EPWM_PRESCALER 1
+
+// EPWM Timer Base counter CLK Divider 3-Bit Divider.
+#define CLK_DIV 1
+
+// High Speed TBClock Prescaler to the ePWM Time Base counter CLK 3-Bit Divider.
+#define HIGH_SPEED_TB_CLK_PRESCALAR 1
+
 // System CLK 100 Mhz
 #define SYSCLK 100000000
 
 /*--------------------------------------------------------------------------------------------------------------------------------
 [Function Name]: EPWM1A_setPeriodAndDuty.
-[Description]: This function is responsible for enabling EPWM1 module and configure PWM period and duty cycle
-                   to generate a PWM signal from EPWM1A module on GPIO 0 pin.
+[Description]: This function is responsible for enabling ePWM1 module and configure PWM period and duty cycle
+                   to generate a PWM signal from ePWM1A module on GPIO 0 pin.
 [Args]:
 [In]        pwmPeriod:
                 Timer Base period value that represents PWM frequency.
-                T(PWM) = pwmPeriod * T(EPWM Timer Base Clock).
+                T(PWM) = (pwmPeriod + 1) * T(ePWM Timer Base Clock).
             pwmDuty:
                 duty cycle ratio from 0(0%) to 1(100%).
 [Returns]   No return.
